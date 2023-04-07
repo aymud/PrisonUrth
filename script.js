5ef90a9043323628f4b9d994f1aa653b3543b612
Ahmed <aaa26770123@gmail.com>
2023-04-07 07:25:44 -0600

    More precise value for num of days in year :o

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 function renderAge() {
-  const birthDate = new Date('1190-04-01T06:23:01Z');
-  const currentDate = Date.now();
-  const timeDiffInMs = currentDate - birthDate.getTime();
+    const birthDateInUTC = new Date('1190-04-01T06:23:01Z');
+    const currentDate = Date.now();
+    const timeDiffInMs = currentDate - birthDateInUTC.getTime();
 
-  const { years, months, days, hours, minutes, seconds, milliseconds } = calculateTimeUnits(timeDiffInMs);
+    const {years, months, days, hours, minutes, seconds, milliseconds} = calculateTimeUnits(timeDiffInMs);
 
-  document.getElementById('years').textContent = padNumber(years, 2);
-  document.getElementById('months').textContent = padNumber(months, 2);
-  document.getElementById('days').textContent = padNumber(days, 2);
-  document.getElementById('hours').textContent = padNumber(hours, 2);
-  document.getElementById('minutes').textContent = padNumber(minutes, 2);
-  document.getElementById('seconds').textContent = padNumber(seconds, 2);
-  document.getElementById('milliseconds').textContent = padNumber(milliseconds, 3);
+    document.getElementById('years').textContent = padNumber(years, 2);
+    document.getElementById('months').textContent = padNumber(months, 2);
+    document.getElementById('days').textContent = padNumber(days, 2);
+    document.getElementById('hours').textContent = padNumber(hours, 2);
+    document.getElementById('minutes').textContent = padNumber(minutes, 2);
+    document.getElementById('seconds').textContent = padNumber(seconds, 2);
+    document.getElementById('milliseconds').textContent = padNumber(milliseconds, 3);
 }
 
 function calculateTimeUnits(timeDiffInMs) {
-  const milliSecondsInYear = 1000 * 60 * 60 * 24 * 365.25;
-  const milliSecondsInMonth = milliSecondsInYear / 12;
-  const milliSecondsInDay = 1000 * 60 * 60 * 24;
-  const milliSecondsInHour = 1000 * 60 * 60;
-  const milliSecondsInMinute = 1000 * 60;
+    const avgLengthOfYearInDays = 365.2425;
+    const milliSecondsInYear = 1000 * 60 * 60 * 24 * avgLengthOfYearInDays;
+    const milliSecondsInMonth = milliSecondsInYear / 12;
+    const milliSecondsInDay = 1000 * 60 * 60 * 24;
+    const milliSecondsInHour = 1000 * 60 * 60;
+    const milliSecondsInMinute = 1000 * 60;
+    const milliSecondsInSecond = 1000;
 
-  const years = Math.floor(timeDiffInMs / milliSecondsInYear);
-  const months = Math.floor((timeDiffInMs % milliSecondsInYear) / milliSecondsInMonth);
-  const days = Math.floor((timeDiffInMs % milliSecondsInMonth) / milliSecondsInDay);
-  const hours = Math.floor((timeDiffInMs % milliSecondsInDay) / milliSecondsInHour);
-  const minutes = Math.floor((timeDiffInMs % milliSecondsInHour) / milliSecondsInMinute);
-  const seconds = Math.floor((timeDiffInMs % milliSecondsInMinute) / 1000);
-  const milliseconds = timeDiffInMs % 1000;
+    const years = Math.floor(timeDiffInMs / milliSecondsInYear);
+    const months = Math.floor((timeDiffInMs % milliSecondsInYear) / milliSecondsInMonth);
+    const days = Math.floor((timeDiffInMs % milliSecondsInMonth) / milliSecondsInDay);
+    const hours = Math.floor((timeDiffInMs % milliSecondsInDay) / milliSecondsInHour);
+    const minutes = Math.floor((timeDiffInMs % milliSecondsInHour) / milliSecondsInMinute);
+    const seconds = Math.floor((timeDiffInMs % milliSecondsInMinute) / milliSecondsInSecond);
+    const milliseconds = timeDiffInMs % milliSecondsInSecond;
 
-  return { years, months, days, hours, minutes, seconds, milliseconds };
+    return {years, months, days, hours, minutes, seconds, milliseconds};
 }
 
 function padNumber(number, width) {
-  return String(number).padStart(width, '0');
+    return String(number).padStart(width, '0');
 }
 
-delayTimeInMs = 1;
-setInterval(renderAge, delayTimeInMs);
+renderTimeInMs = 1;
+setInterval(renderAge, renderTimeInMs);
